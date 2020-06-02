0bfa71c8b090bc6d20ac91750296eec41938348a
frystal <44425340+frystal@users.noreply.github.com>
2020-06-02 11:29:24 +0800

    update

--- src/configs.py
+++ src/configs.py
@@ -7,22 +7,32 @@ if(os.name=='poxis'):
     VALIDATION_IMAGES_PATH = '../data/valid'
     CHECKPOINTS_PATH = '../checkpoints/cp.ckpt'
     SERIALIZE_PATH = './tmp.data'
+    INPUT_IMAGE_PATH = './test_image/original.png'
+    OUTPUT_IMAGE_PATH = './test_image/2x.png'
 elif(os.name=='nt'):
     ORIGINAL_IMAGES_PATH = '..\\data\\original'
     TRAINING_IMAGES_PATH = '..\\data\\train'
     VALIDATION_IMAGES_PATH = '..\\data\\valid'
     CHECKPOINTS_PATH = '..\\checkpoints\\cp.ckpt'
     SERIALIZE_PATH = '.\\tmp.data'
+    INPUT_IMAGE_PATH = '.\\test_image\\original.png'
+    OUTPUT_IMAGE_PATH = '.\\test_image\\2x.png'
 else:
     ORIGINAL_IMAGES_PATH = './data/original'
     TRAINING_IMAGES_PATH = './data/train'
     VALIDATION_IMAGES_PATH = './data/valid'
     CHECKPOINTS_PATH = './checkpoints/cp.ckpt'
     SERIALIZE_PATH = './tmp.data'
+    INPUT_IMAGE_PATH = './test_image/original.png'
+    OUTPUT_IMAGE_PATH = './test_image/2x.png'
     
 DEFAULT_MODEL = 'vgg_7'
 BATCH_SIZE = 8
 STEPS_PER_EPOCH = 5000
+
+OUTPUT_IMAGE_SIZE = 64
+INPUT_IMAGE_SIZE = 32
+SCALE = 2
 
 
 
---
+++ src/start.py
@@ -0,0 +1,50 @@
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import cv2 
+import matplotlib.pyplot as plt
+import math
+from configs import *
+import model
+
+def main():
+    # 载入模型参数
+    targrt_model =  model.choos_model(DEFAULT_MODEL)
+    targrt_model.load_weights(CHECKPOINTS_PATH)
+    
+    # 读入测试图片
+    input_low_image = cv2.imread(INPUT_IMAGE_PATH)
+    # 转为浮点数
+    input_low_image = input_low_image / 255
+    
+
+    input_image_rows = input_low_image.shape[0]
+    input_image_cols = input_low_image.shape[1]
+    input_image_chns = input_low_image.shape[2]
+
+    output_image_rows = math.ceil(input_image_rows / INPUT_IMAGE_SIZE) * OUTPUT_IMAGE_SIZE
+    output_image_cols = math.ceil(input_image_cols / INPUT_IMAGE_SIZE) * OUTPUT_IMAGE_SIZE
+
+    output_high_image = np.zeros(((output_image_rows, output_image_cols, input_image_chns)))
+    test_low_image = np.zeros((1, INPUT_IMAGE_SIZE, INPUT_IMAGE_SIZE, input_image_chns))
+    
+    # 切分图像依次放大
+    for y in range(0, input_image_rows, INPUT_IMAGE_SIZE):
+        for x in range(0, input_image_cols, INPUT_IMAGE_SIZE):
+            test_low_image[0, ...] = input_low_image[y : y + INPUT_IMAGE_SIZE, x : x + INPUT_IMAGE_SIZE, ...]
+            test_high_image = targrt_model.predict(test_low_image)
+
+            output_high_image[2*y : 2*y + OUTPUT_IMAGE_SIZE, 2*x : 2*x + OUTPUT_IMAGE_SIZE, ...] = test_high_image[0, ...]
+
+
+
+    cv2.imwrite(OUTPUT_IMAGE_PATH,output_high_image*255)
+
+    print('-'*150)
+    print('!OK!')
+
+
+
+if __name__ == '__main__':
+    main()
+    
